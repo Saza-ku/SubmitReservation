6be98bb6f33ab31f783b13bbdec7628d190a7670
yutorse <yutohorse.coc@gmail.com>
2020-10-17 18:47:42 +0900

    緊急commit

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ app = Flask(__name__)  # アプリの設定
 def main():
     return "Hello, World!"  # Hello, World! を出力
 
+
+
+
 if __name__ == "__main__":  # 実行されたら
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)  # デバッグモード、localhost:8888 で スレッドオンで実行
