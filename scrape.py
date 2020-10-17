341a95948d01128ada6a2cad9f98c9199a006766
saza-ku <uhkr58057@mineo.jp>
2020-10-17 17:15:12 +0900

    インデントを直した。

--- scrape.py
+++ scrape.py
@@ -41,26 +41,26 @@ def login(user, pwd):
 
 def get_cources(browser):
 	dashboard = browser.find_element_by_link_text("サイトセットアップ")
-  	dashboard.click()
-  	sleep(2)
-  	cources = browser.find_elements_by_tag_name("tr")
+	dashboard.click()
+	sleep(2)
+	cources = browser.find_elements_by_tag_name("tr")
 
-  	urls = []
-  	for cource in cources:
+	urls = []
+	for cource in cources:
 		urls = cource.find_elements_by_tag_name('a')
-      	for a in urls:
-          	url = a.get_attribute("href")
+		for a in urls:
+			url = a.get_attribute("href")
 
-          	"""
-          	下のif文の意図を書きます。
-          	urlsに入っているaタグのhrefは二つあります。
-          	1つは講義のURLでもう1つはhref="#"です。
-          	href="#"が講義のURLリストに入るのは避けたいので下のif文を書きました。
-          	"""
+			"""
+			下のif文の意図を書きます。
+			urlsに入っているaタグのhrefは二つあります。
+			1つは講義のURLでもう1つはhref="#"です。
+			href="#"が講義のURLリストに入るのは避けたいので下のif文を書きました。
+			"""
 
-# "#_URL"にすると手元の環境だと動かない。サイトセットアップのページに戻るURLを除去したいので、そのURLをそのまま書く方が確実かと？
-		if url == "#_URL": #urlsには講義のURLともう一つ別のURLが入っています。後者を避けるためのif文です。
-        	continue
+			# "#_URL"にすると手元の環境だと動かない。サイトセットアップのページに戻るURLを除去したいので、そのURLをそのまま書く方が確実かと？
+			if url == "#_URL": #urlsには講義のURLともう一つ別のURLが入っています。後者を避けるためのif文です。
+				continue
 		urls.append(url)
 	return cources
 
